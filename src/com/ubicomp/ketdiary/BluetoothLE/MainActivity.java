2b1a37c732c8a114e8a5477ef98a66454a934a28
Kelvin <kelvindk@yahoo.com>
2015-05-20 02:25:33 +0800

    add BLE componen, MainActivity.java as example for manipulating BLE

---
+++ src/com/ubicomp/ketdiary/BluetoothLE/MainActivity.java
@@ -0,0 +1,139 @@
+package ubicomp.bioscope.main;
+
+
+import ubicomp.bioscope.R;
+import ubicomp.bioscope.main.BluetoothLE.*;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends ListActivity implements BluetoothListener {
+
+    private static final String TAG = "BluetoothLE";
+
+	private BluetoothLE ble = null;
+    MainActivity mainActivity = this;
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+
+        Button buttonStart = (Button)findViewById(R.id.buttonStart);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if(ble != null) {
+                    return;
+                }
+                ble = new BluetoothLE(mainActivity, "KetDiary-Kel");
+                ble.bleConnect();
+            }
+
+        });
+
+        Button buttonClose = (Button)findViewById(R.id.buttonClose);
+
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if(ble != null) {
+                    ble.bleDisconnect();
+                    ble = null;
+                }
+            }
+
+        });
+
+	}
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(ble != null) {
+            ble.bleDisconnect();
+        }
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        ble.onBleActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void bleNotSupported() {
+
+//        this.finish();
+    }
+
+    @Override
+    public void bleConnectionTimeout() {
+        Toast.makeText(this, "BLE connection timeout", Toast.LENGTH_SHORT).show();
+        if(ble != null) {
+            ble = null;
+        }
+    }
+
+    @Override
+    public void bleConnected() {
+        Log.i(TAG, "BLE connected");
+    }
+
+    @Override
+    public void bleDisconnected() {
+        Log.i(TAG, "BLE disconnected");
+        if(ble != null) {
+            ble = null;
+        }
+    }
+
+    @Override
+    public void bleWriteStateSuccess() {
+        Log.i(TAG, "BLE ACTION_DATA_WRITE_SUCCESS");
+    }
+
+    @Override
+    public void bleWriteStateFail() {
+        Log.i(TAG, "BLE ACTION_DATA_WRITE_FAIL");
+    }
+
+    @Override
+    public void bleNoPlug() {
+        Log.i(TAG, "No test plug");
+    }
+
+    @Override
+    public void blePlugInserted(byte[] plugId) {
+        Log.i(TAG, "Test plug is inserted");
+    }
+
+    @Override
+    public void bleConductiveElectrode1(byte[] adcValue) {
+
+    }
+
+    @Override
+    public void bleConductiveElectrode2(byte[] adcValue) {
+
+    }
+
+    @Override
+    public void bleColorReadings(byte[] colorReadings) {
+        Log.i(TAG, "Color sensor readings");
+    }
+}
