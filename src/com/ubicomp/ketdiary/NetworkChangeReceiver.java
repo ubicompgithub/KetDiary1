d17858078c903f5cc01f13fdbbb35fd58e222030
Andy-Ubicomp <andy860106@hotmail.com>
2015-05-24 22:18:03 +0800

    - add BootBoarcastReceiver and NetworkChangeReceiver.

---
+++ src/com/ubicomp/ketdiary/NetworkChangeReceiver.java
@@ -0,0 +1,41 @@
+package com.ubicomp.ketdiary;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+import com.ubicomp.ketdiary.system.NetworkCheck;
+
+/**
+ * Receiver for getting the network condition
+ * 
+ * @author Stanley Wang
+ */
+public class NetworkChangeReceiver extends BroadcastReceiver {
+
+	private static final String TAG = "NETWORK_CHANGE_RECEIVER";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+
+		String action = intent.getAction();
+		Log.d(TAG, action);
+
+		if (!action.equals(ConnectivityManager.CONNECTIVITY_ACTION)
+				&& !action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION))
+			return;
+
+		if (!NetworkCheck.networkCheck()) {
+			Log.d(TAG, "NOT CONNECTED");
+			return;
+		}
+
+		Intent regularIntent = new Intent(context, UploadService.class);
+		context.startService(regularIntent);
+
+	}
+
+}
