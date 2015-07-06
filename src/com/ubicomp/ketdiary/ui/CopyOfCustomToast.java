6d59d019a50bc85462f2b01d496bdf4dd7410836
unknown <jimmy830809@gmail.com>
2015-07-06 16:54:57 +0800

    重剪了一次自我和他人的心情圖案 typepager.xml位置重新調整 新增一個toast.xml 並且其頁的一些圖片 CopyOfCustomToast為原本的CustomToast 當前版本的CustomToast執行仍然會崩潰 待修

--- src/com/ubicomp/ketdiary/ui/CopyOfCustomToast.java
+++ src/com/ubicomp/ketdiary/ui/CopyOfCustomToast.java
@@ -14,19 +14,16 @@ import android.widget.Toast;
 import com.ubicomp.ketdiary.App;
 import com.ubicomp.ketdiary.R;
 
-public class CustomToast {
+public class CopyOfCustomToast {
 
 	private static Toast toast = null;
 
 	private static View layout = null;
-//	private static TextView toastText, toastCounter;
-//	private static ImageView toastImage;
-	
-	private static ImageView toastCircle, toastContent;
-	private static TextView toastDescribe;
+	private static TextView toastText, toastCounter;
+	private static ImageView toastImage;
 
-	private static Drawable c1Drawable, c2Drawable, c3Drawable, okDrawable;
-			//failDrawable;
+	private static Drawable c1Drawable, c2Drawable, c3Drawable, okDrawable,
+			failDrawable;
 
 	private static SoundPool soundpool;
 	private static int soundId;
@@ -47,21 +44,14 @@ public class CustomToast {
 		if (layout == null) {
 			LayoutInflater inflater = (LayoutInflater) context
 					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			layout = inflater.inflate(R.layout.toast, null);
-//			toastText = (TextView) layout.findViewById(R.id.custom_toast_text);
-//			toastText.setTypeface(Typefaces.getWordTypefaceBold());
-//			toastCounter = (TextView) layout
-//					.findViewById(R.id.custom_toast_counter);
-//			toastCounter.setTypeface(Typefaces.getWordTypefaceBold());
-//			toastImage = (ImageView) layout
-//					.findViewById(R.id.custom_toast_main_picture);
-			
-			toastDescribe= (TextView) layout.findViewById(R.id.toast_description);
-			toastDescribe.setTypeface(Typefaces.getWordTypefaceBold());
-			toastContent=(ImageView) layout
-					.findViewById(R.id.toast_circlecontent);
-//			toastCircle=(ImageView) layout
-//					.findViewById(R.id.toast_circle);
+			layout = inflater.inflate(R.layout.toast_full_screen, null);
+			toastText = (TextView) layout.findViewById(R.id.custom_toast_text);
+			toastText.setTypeface(Typefaces.getWordTypefaceBold());
+			toastCounter = (TextView) layout
+					.findViewById(R.id.custom_toast_counter);
+			toastCounter.setTypeface(Typefaces.getWordTypefaceBold());
+			toastImage = (ImageView) layout
+					.findViewById(R.id.custom_toast_main_picture);
 			c1Drawable = context.getResources().getDrawable(
 					R.drawable.toast_add_1);
 			c2Drawable = context.getResources().getDrawable(
@@ -69,9 +59,9 @@ public class CustomToast {
 			c3Drawable = context.getResources().getDrawable(
 					R.drawable.toast_add_3);
 			okDrawable = context.getResources().getDrawable(
-					R.drawable.toast_goodjob);
-//			failDrawable = context.getResources().getDrawable(
-//					R.drawable.toast_fail);
+					R.drawable.toast_pass);
+			failDrawable = context.getResources().getDrawable(
+					R.drawable.toast_fail);
 		}
 		if (soundpool == null) {
 			soundpool = new SoundPool(1, AudioManager.STREAM_MUSIC, 1);
@@ -81,32 +71,32 @@ public class CustomToast {
 		toast.setMargin(0, 0);
 		toast.setGravity(Gravity.FILL, 0, 0);
 		toast.setDuration(Toast.LENGTH_SHORT);
-		toastDescribe.setText(str_id);
+		toastText.setText(str_id);
 
 		switch (counter) {
 		case 0:
-			toastContent.setImageDrawable(okDrawable);
-			//toastCounter.setVisibility(View.INVISIBLE);
+			toastImage.setImageDrawable(okDrawable);
+			toastCounter.setVisibility(View.INVISIBLE);
 			break;
 		case 1:
-			toastContent.setImageDrawable(c1Drawable);
-			//toastCounter.setVisibility(View.VISIBLE);
+			toastImage.setImageDrawable(c1Drawable);
+			toastCounter.setVisibility(View.VISIBLE);
 			soundpool.play(soundId, 1.f, 1.f, 0, 0, 1.f);
 			break;
 		case 2:
-			toastContent.setImageDrawable(c2Drawable);
-			//toastCounter.setVisibility(View.VISIBLE);
+			toastImage.setImageDrawable(c2Drawable);
+			toastCounter.setVisibility(View.VISIBLE);
 			soundpool.play(soundId, 1.f, 1.f, 0, 0, 1.f);
 			break;
 		case 3:
-			toastContent.setImageDrawable(c3Drawable);
-			//toastCounter.setVisibility(View.VISIBLE);
+			toastImage.setImageDrawable(c3Drawable);
+			toastCounter.setVisibility(View.VISIBLE);
 			soundpool.play(soundId, 1.f, 1.f, 0, 0, 1.f);
 			break;
-//		case -1:
-//			toastImage.setImageDrawable(failDrawable);
-//			toastCounter.setVisibility(View.INVISIBLE);
-//			break;
+		case -1:
+			toastImage.setImageDrawable(failDrawable);
+			toastCounter.setVisibility(View.INVISIBLE);
+			break;
 		}
 
 		toast.show();
